59c43a34865284c34cb1533c512b2379e7f4b87a
Pucci <greenymanxcreative@gmail.com>
2024-06-26 16:02:34 +0300

    Обновил до версии дев

--- bot.py
+++ bot.py
@@ -4,8 +4,6 @@ from text import *
 
 bot = telebot.TeleBot('7232178964:AAHYKJvU_c2t15_RjFT6WOa78yLZ37ZDUaY')
 
-ADMIN_CHANNEL_ID = '-1002147985788'
-
 user_states = {}
 
 user_question = ''
@@ -14,6 +12,13 @@ def information_button():
     markup = types.InlineKeyboardMarkup()
     inf_button = types.InlineKeyboardButton('Узнать больше', callback_data='information')
     markup.add(inf_button)
+    return markup
+
+
+def create_feedback_markup():
+    markup = types.InlineKeyboardMarkup()
+    back_button = types.InlineKeyboardButton("Назад", callback_data='back_to_menu')
+    markup.add(back_button)
     return markup
 
 
@@ -27,6 +32,25 @@ def create_buttons_markup():
     markup.add(payment_info, format_info)
     markup.add(schedule_info, parents_info)
     markup.add(summer_info)
+    return markup
+
+
+def create_schedule_markup():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    group = types.InlineKeyboardButton("Групповые занятия", callback_data='group')
+    mentoring = types.InlineKeyboardButton("Наставничество", callback_data='mentoring')
+    back = types.InlineKeyboardButton("Назад", callback_data='back_to_menu')
+    markup.add(group)
+    markup.add(mentoring)
+    markup.add(back)
+    return markup
+
+
+def create_schedule_info_markup():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    back_button = types.InlineKeyboardButton("Назад", callback_data='back_to_schedule')
+    feedback_button = types.InlineKeyboardButton('Обратная связь', callback_data='feedback')
+    markup.add(back_button, feedback_button)
     return markup
 
 
@@ -65,9 +89,7 @@ def create_summer_markup():
 
 def create_pay_markup():
     markup=types.InlineKeyboardMarkup(row_width=2)
-    how_to_pay = types.InlineKeyboardButton("Как происходит оплата",callback_data="how_to_pay")
-    education_info = types.InlineKeyboardButton("Как происходит обучение",callback_data="education_info")
-    support_info = types.InlineKeyboardButton("Кураторская поддержка",callback_data="support_info")
+    how_to_pay = types.InlineKeyboardButton("Как происходит оплата", callback_data="how_to_pay")
     back = types.InlineKeyboardButton("Назад", callback_data='back_to_menu')
     markup.add(how_to_pay)
     markup.add(education_info)
@@ -84,10 +106,43 @@ def create_pay_info_markup():
     return markup
 
 
-def create_feedback_markup():
-    markup = types.InlineKeyboardMarkup()
-    back_button = types.InlineKeyboardButton("Назад", callback_data='back_to_menu')
-    markup.add(back_button)
+def create_format_markup():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    education_info = types.InlineKeyboardButton("Как происходит обучение", callback_data="education_info")
+    support_info = types.InlineKeyboardButton("Кураторская поддержка", callback_data="support_info")
+    back = types.InlineKeyboardButton("Назад", callback_data='back_to_menu')
+    markup.add(education_info)
+    markup.add(support_info)
+    markup.add(back)
+    return markup
+
+
+def create_info_format_markup():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    back_button = types.InlineKeyboardButton("Назад", callback_data='back_to_format')
+    feedback_button = types.InlineKeyboardButton('Обратная связь', callback_data='feedback')
+    markup.add(back_button, feedback_button)
+    return markup
+
+
+def create_parents_markup():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    general_information = types.InlineKeyboardButton("Общая информация", callback_data="general_information")
+    soft_skills_after_training = types.InlineKeyboardButton("Soft Skills после обучения", callback_data="soft_skills_after_training")
+    hard_skills_after_training = types.InlineKeyboardButton("Hard skills после обучения", callback_data='hard_skills_after_training')
+    back = types.InlineKeyboardButton("Назад", callback_data='back_to_menu')
+    markup.add(general_information)
+    markup.add(soft_skills_after_training)
+    markup.add(hard_skills_after_training)
+    markup.add(back)
+    return markup
+
+
+def create_info_parents_markup():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    back_button = types.InlineKeyboardButton("Назад", callback_data='back_to_parents')
+    feedback_button = types.InlineKeyboardButton('Обратная связь', callback_data='feedback')
+    markup.add(back_button, feedback_button)
     return markup
 
 
@@ -104,63 +159,48 @@ def callback(call):
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Теперь выберите один из вариантов или свяжитесь с администрацией:", reply_markup=buttons_markup)
     elif call.data == 'info_payment':
         bot.answer_callback_query(call.id)
-        pay_markup = create_pay_markup()
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Информация про оплату.", reply_markup=pay_markup)
-    elif call.data == 'how_to_pay':
-        bot.answer_callback_query(call.id)
-        info_markup = create_pay_info_markup()
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="""Вы оплачиваете каждый период/абонемент (8 занятий по утвержденному расписанию), каждые 28 дней, в последний день предыдущего оплаченного периода. Мы будем об этом напоминать. Если нет возможности оплатить в срок предупредите, мы подождем, занятия при этом не приостанавливаем.
-Новогодние, майские и летние каникулы исключение,
-когда абонемент можно заморозить. В остальное время оплата идет каждые 8 занятий по расписанию.
-
-Пропущенные основные занятия по любым причинам у вас не сгорают, а отрабатываются, вне расписания, по договоренности с педагогом, без привязке к оплате периодов. По запросу мы пришлем вам варианты, когда можно подключиться на отработку. Отработки проводятся в другой группе при наличии свободного места, либо индивидуально, но длительностью 45 минут. Если нет возможности отработать в учебное время, то копим пропуски и отрабатываем во время школьных каникул.
-
-При покупке нескольких периодов предоставляем
-скидки. Когда по вашей рекомендации к нам придет новый ученик - фиксированная скидка 2500 рублей на один период.
-
-При решении сделать паузу в занятиях более трех недель без оплаты будущих уроков место в группе открепляется, и по возвращению если место занято - подбирается новая группа или педагог.""", reply_markup=info_markup)
-    elif call.data == 'education_info':
-            bot.answer_callback_query(call.id)
-            info_markup = create_pay_info_markup()
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="""Ребята от 9 до 17 обучаются в мини-группах до 4-х человек. С ними наставник-педагог.
-    Программа состоит из модулей, в модулях лежат проекты, проекты поделены на шаги, в шаги вложена необходимая информация (статьи, ссылки, документация), необходимая к изучению для
-    выполнения шага.
-
-    По завершению проект отправляется на проверку. Проверяют действующие программисты и пишут ревью-код. После принятия открывается следующий проект модуля. Если пришли правки, то нужно доработать проект. Время проверки - от 3 до 24 часов. Пока проверяется проект одного модуля, выполняем проект другого модуля программы.
-
-    Педагог будет помогать с возникшими вопросами, следить за происходящим, комментировать, объяснять при трудностях, курировать задачи.
-
-    Материалы на платформе в доступен и вне занятий. Домашнее задание обычно - несколько шагов самостоятельно. От самостоятельной работы напрямую зависит скорость прогресса. Больше уделяет времени программированию - быстрее проходит программу. На урок можно подключаться уже с возникшими вопросами по задачам.""", reply_markup=info_markup)
-    elif call.data == 'support_info':
-            bot.answer_callback_query(call.id)
-            info_markup = create_pay_info_markup()
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="""Спасибо Вам, что выбрали нашу школу и доверили своего ребенка на обучение. Мы будем прикладывать все усилия, чтобы дать максимальный результат и добиться поставленных Вами целей
-
-Что дальше?
-Дальше у нас наступает первый месяц адаптации. Месяц, когда ученик знакомится ближе с педагогом, правилами группы, форматом обучения, своими обязанностями, и самое главное - начинает сталкиваться с первыми трудностями.	
-
-Наша совместная задача, родителя и куратора - держать руку на пульсе и сразу реагировать на все замечания и комментарии ребенка на любые моменты
-образовательного процесса. Это позволит понимать ситуацию и вносить изменения в работу, что снизит стресс у ребенка и поможет быстрее влиться в процесс.
-
-Пишите пожалуйста, обратную связь и не бойтесь критиковать.
-По всем вопросам - обращайтесь, с удовольствием
-ответим.
-
-Мы здесь на связи с Вами с 9 до 20 по Москве в будни и с 10 до 19 в выходные дни. Куратор при необходимости будет Вам звонить, обсуждать процесс обучения.
-
-""", reply_markup=info_markup)
+        info_markup = create_info_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=how_to_pay, reply_markup=info_markup)
     elif call.data == 'info_format':
         bot.answer_callback_query(call.id)
-        info_markup = create_info_markup()
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Информация про формат обучения.", reply_markup=info_markup)
+        format_markup = create_format_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Информация про формат обучения.", reply_markup=format_markup)
+    elif call.data == 'education_info':
+        bot.answer_callback_query(call.id)
+        info_markup = create_info_format_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=education_info, reply_markup=info_markup)
+    elif call.data == 'support_info':
+        bot.answer_callback_query(call.id)
+        info_markup = create_info_format_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=support_info, reply_markup=info_markup)
     elif call.data == 'info_schedule':
         bot.answer_callback_query(call.id)
-        info_markup = create_info_markup()
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Информация про расписание.", reply_markup=info_markup)
+        schedule_markup = create_schedule_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Информация про расписание.", reply_markup=schedule_markup)
+    elif call.data == 'group':
+        bot.answer_callback_query(call.id)
+        info_markup = create_schedule_info_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=group, reply_markup=info_markup)
+    elif call.data == 'mentoring':
+        bot.answer_callback_query(call.id)
+        info_markup = create_schedule_info_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=mentoring, reply_markup=info_markup)
     elif call.data == 'info_parents':
         bot.answer_callback_query(call.id)
-        info_markup = create_info_markup()
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Информация для родителей.", reply_markup=info_markup)
+        parents_markup = create_parents_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Информация для родителей.", reply_markup=parents_markup)
+    elif call.data == 'general_information':
+        bot.answer_callback_query(call.id)
+        info_markup = create_info_parents_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=general_information, reply_markup=info_markup)
+    elif call.data == 'soft_skills_after_training':
+        bot.answer_callback_query(call.id)
+        info_markup = create_info_parents_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=soft_skills_after_training, reply_markup=info_markup)
+    elif call.data == 'hard_skills_after_training':
+        bot.answer_callback_query(call.id)
+        info_markup = create_info_parents_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=hard_skills_after_training, reply_markup=info_markup)
     elif call.data == 'info_summer':
         bot.answer_callback_query(call.id)
         summer_markup = create_summer_markup()
@@ -201,6 +241,18 @@ def callback(call):
         bot.answer_callback_query(call.id)
         summer_markup = create_summer_markup()
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Теперь выберите один из вариантов или свяжитесь с администрацией:", reply_markup=summer_markup)
+    elif call.data == 'back_to_format':
+        bot.answer_callback_query(call.id)
+        format_markup = create_format_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Теперь выберите один из вариантов или свяжитесь с администрацией:", reply_markup=format_markup)
+    elif call.data == 'back_to_parents':
+        bot.answer_callback_query(call.id)
+        parents_markup = create_parents_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Теперь выберите один из вариантов или свяжитесь с администрацией:", reply_markup=parents_markup)
+    elif call.data == 'back_to_schedule':
+        bot.answer_callback_query(call.id)
+        schedule_markup = create_schedule_markup()
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Теперь выберите один из вариантов или свяжитесь с администрацией:", reply_markup=schedule_markup)
     elif call.data == 'back_to_menu':
         bot.answer_callback_query(call.id)
         buttons_markup = create_buttons_markup()
